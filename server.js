15c912122a254d67c27257ad3e85dabdf463c89f
Sherzod-1998 <sherzod.oltinsoy@gmail.com>
2024-11-14 20:04:47 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 // Web Serverni boshlash
 console.log("Web Serverni boshlash");
-
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
-// 1: Static va JSON middleware
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
+
 app.use(express.static("public")); // Static fayllar uchun papka
 app.use(express.json()); // JSON formatidagi ma'lumotlarni o'qish
 app.use(express.urlencoded({ extended: true })); // URL encoded ma'lumotlarni o'qish
@@ -18,13 +26,17 @@ app.set("view engine", "ejs");
 
 // 4: Routing
 app.post("/creatr-item", (req, res) => {
-    console.log(req.body);
-    res.json({test:"success"});
+    //TODO: code with db here
 });
+
+app.get('/author' , (req, res) =>{
+    res.render("author", {user: user});
+})
 
 app.get("/", function(req, res){
     res.render("harid");
 });
+
 
 // Server yaratish va tinglash
 const server = http.createServer(app);
